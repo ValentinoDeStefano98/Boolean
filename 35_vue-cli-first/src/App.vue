791d6fdf2f2948a4440d5421375b523064124fe9
Valentino De Stefano <valentino.destefano46@gmail.com>
2023-01-20 15:57:07 +0100

    Added vue-cli-first

---
+++ 35_vue-cli-first/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <TitoloComponente></TitoloComponente>
+    <JumboComp></JumboComp>
+  </div>
+</template>
+
+<script>
+
+import TitoloComponente from './components/TitoloComponente.vue'
+import JumboComp from './components/JumboComp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TitoloComponente,
+    JumboComp
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 35_vue-cli-first/src/components/JumboComp.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div class="jumbo">
+
+        </div>      
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'JumboComp',
+    }
+</script>
+
+<style scoped>
+.jumbo{
+    height: 400px;
+    width: 100%;
+    background-image: url(./../../public/ny.jpeg);
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+</style>

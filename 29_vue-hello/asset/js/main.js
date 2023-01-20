4f709e7419ec0e01ba9679ffa3e69bc60e0d71a3
Valentino De Stefano <valentino.destefano46@gmail.com>
2023-01-20 15:44:49 +0100

    Added vue-hello

---
+++ 29_vue-hello/asset/js/main.js
@@ -0,0 +1,10 @@
+//Carico Vue
+var app = new Vue(
+    {
+        el: '#root' ,
+        data: {
+            message: 'ciao',
+            photo: './asset/img/grand-canyon-1.jpeg',
+        }
+    }
+);

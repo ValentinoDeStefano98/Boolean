459a06db2fdf0e38c0480287bade096c42f6268d
Valentino De Stefano <valentino.destefano46@gmail.com>
2023-01-20 15:17:46 +0100

    Added js-pwdgen-wannabe

---
+++ 16_js-pwdgen-wannabe/asset/js/main.js
@@ -0,0 +1,9 @@
+let nomeUtente = prompt("Ciao! Inserisici il tuo nome");
+
+let cognomeUtente = prompt("Ora inserisci il tuo cognome");
+
+let colorePreferito = prompt("Infine inserisci il tuo colore preferito");
+
+const passwordNumber = 21;
+
+document.getElementById("contenitore_target").innerHTML = nomeUtente + cognomeUtente + colorePreferito + passwordNumber;
